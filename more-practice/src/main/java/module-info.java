5c77225c4db92f8a325707b9c179fb7102f9bac0
Nik <nikitama1hr@gmail.com>
2023-08-30 16:54:28 +0300

    brain ache with java compiler versions

--- more-practice/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.morepractice {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.morepractice to javafx.fxml;
-    exports com.example.morepractice;
-}
